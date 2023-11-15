f16e5785f5667ae32dc4f5f3574c626490b63386
fabio-coutinho-dos-santos <fabio.santcou@gmail.com>
2023-11-14 22:51:37 -0300

    chore: add swagger module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = new DocumentBuilder()
+    .setTitle('Shaw and Partners API')
+    .setDescription('')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup(`api/doc`, app, document);
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(process.env.SERVER_PORT);
 }
 bootstrap();
