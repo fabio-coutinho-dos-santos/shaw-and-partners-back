dc43b0de8928881a7529ee1716efafeaf36542f3
fabio-coutinho-dos-santos <fabio.santcou@gmail.com>
2023-11-14 23:00:52 -0300

    chore: add cors policy

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,18 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    next();
+  });
+
+  app.enableCors({
+    allowedHeaders: '*',
+    origin: '*',
+  });
+
   SwaggerModule.setup(`api/doc`, app, document);
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.SERVER_PORT);
